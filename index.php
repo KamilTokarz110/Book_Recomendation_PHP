9eb34ea44f8270990bb9f7d62ef76f1d85f84af8
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-06-23 10:59:15 +0200

    Create header.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'header.php'
+?> 
+<div class="wrapper">
+        <div class="slider">slider</div>
+        <section class="main-articles">main-article</section>
+        <section class="book-list">book-list</section>
+        <footer class="footer">footer</footer>
+    </div>
+        
+    
+</body>
+<script src="main.js"></script>
+</html>
