aebeac504a4dec741546b6efb976a5b715c60bdc
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-06-25 07:33:48 +0200

    Creating signup page error messages

--- signup.php
+++ signup.php
@@ -17,7 +17,30 @@ include_once 'header.php'
         </form>
         </section>
 
+<?php
 
+if(isset($_GET["error"])){
+        if($_GET["error"] == "emptyinput"){
+                echo "<p>Fill in all fields! </p>";
+        }
+        else if($_GET["error"]=="invaliduid"){
+                echo "<p>Choose a proper username!</p>";
+        }
+        else if($_GET["error"]=="invalidemail"){
+                echo "<p>Choose a proper email!</p>";
+        }
+        else if($_GET["error"]=="passwordsdontmatch"){
+                echo "<p>Password doesn't match!</p>";
+        } else if($_GET["error"]=="stmtfield"){
+                echo "<p>Something went wrong, try again!</p>";
+        } else if($_GET["error"]=="usernametaken"){
+                echo "<p>Username already taken</p>";
+        } else if($_GET["error"]=="none"){
+                echo "<p>You have signed up!</p>";
+        }
+}
+
+?>
        
 <?php
 include_once 'footer.php'
