15ad5c644585ff99e295b9bab51181fdb23200aa
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-07-09 10:55:57 +0200

    Styling about and feature sections

--- header.php
+++ header.php
@@ -4,13 +4,13 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Recomendation</title>
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -9,9 +9,52 @@ if(isset($_SESSION["useruid"])){
     }
     
     ?>
-        <div class="slider">slider</div>
-        <section class="main-articles">main-article</section>
-        <section class="book-list">book-list</section>
+    <div class="banner">
+        
+        <div class="banner__title">
+            <h1 class="banner__title-text">What should i<br>read next</h1>
+            <h2 class="banner__title-text"></h2>
+        </div>
+    </div>
+    <section class="about">
+    <div class="about__container">
+        <h1 class="about__title">About Us</h1> 
+        <p class="about__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo voluptatibus, cumque quaerat eveniet veniam reiciendis voluptatem dolor doloribus qui eaque incidunt repudiandae laborum numquam tempore ratione? Error ratione magni neque!
+        </p>
+        <div class="about__items">
+        <span>Item_1</span>
+        <span>Item_2</span>
+        <span>Item_3</span>
+
+        </div>
+
+        </div>
+    </section>
+    <section class="feature">
+        
+        <div class="feature__item">
+        <i class="fas fa-user feature__item-image"></i>
+            <h1 class="feature__item-title">Register</h1>
+            <p class="feature__item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptatum neque laborum! Laborum quibusdam cumque labore molestias ullam saepe porro facere unde blanditiis dicta iusto necessitatibus, quaerat consequuntur distinctio amet.</p>
+        </div>
+        <div class="feature__item--arrow">
+        <i class="fas fa-arrow-right"></i>
+        </div>
+        <div class="feature__item">
+        <i class="fas fa-book-reader feature__item-image"></i>
+            <h1 class="feature__item-title">Read</h1>
+            <p class="feature__item-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit velit cum eaque nostrum error hic minima mollitia quas et quis ipsam dolorum, enim accusamus, accusantium cupiditate voluptate? Eligendi, neque nemo!</p>
+        </div>
+        <div class="feature__item--arrow">
+        <i class="fas fa-arrow-right"></i>
+        </div>
+        <div class="feature__item">
+        <i class="fab fa-slideshare feature__item-image"></i>
+            <h1 class="feature__item-title">Share</h1>
+            <p class="feature__item-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit minima incidunt atque doloremque veritatis consectetur inventore optio accusamus veniam, ducimus a debitis quas dolores autem odit molestias nam et? Aperiam!</p>
+        </div>
+        
+    </section>
        
 <?php
 include_once 'footer.php'
